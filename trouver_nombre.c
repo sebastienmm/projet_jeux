261f78038a7e8294ca4973164f733711376705e5
sebastienmm <sebastien_meylan@orange.fr>
2017-12-16 16:57:13 +0100

    Modification du fichier trouver_nombre.c, ajout d'une boucle et de 3 conditions

--- trouver_nombre.c
+++ trouver_nombre.c
@@ -5,25 +5,37 @@
 int main (int argc, char *argv[])
 {
 	//initialisation et création d'un nombre aléatoire
-	printf "Calcul du nombre mystère";
-	const int MAX=100, MIN=1 ;
-	srand=( time ( NULL )) ;
-	nombreMystere = ( rand() % ( MAX - MIN + 1 )) + MIN ;
-	
-	printf "L'ordinateur a calculé un nombre mystère.\n A toi de jouer pour le trouver !! ";
-	Printf "Fais une première proposition comprise entre 1 et 100"
+	int nombreMystere=0;
 	int proposition=0;
-	while (nombremystère != proposition)
+
+	const int MAX=100, MIN=1 ;
+	srand( time ( NULL )) ;
+	nombreMystere=( rand() % ( MAX - MIN + 1 )) + MIN ;
+	
+	printf ("L'ordinateur a calculé un nombre mystère.\n A toi de jouer pour le trouver !!\n ");
+	printf ("Fais une première proposition comprise entre 1 et 100\n");
+	
+	while (nombreMystere != proposition )
 	{
 		scanf ("%d", &proposition);
 		if (nombreMystere < proposition)
 		{
-			printf "Le nombre mystère est plus petit,\n nouvelle proposition:";
+			printf ("Le nombre mystère est plus petit,\n nouvelle proposition:\n");
+		}
+		else if(nombreMystere > proposition)
+		{
+			printf ("Le nombre mystère est plus grand,\n nouvelle proposition:\n");
+		}
+		else if( nombreMystere > proposition )
+
+		{
+			printf ("Bravo tu as trouvé le nombre mystère, il est égal à %d\n",proposition);
 		}
 		else
 		{
-			printf "Le nombre mystère est plus grand,\n nouvelle proposition:";
+			printf (" ");
 		}
 	}
-	printf "Bravo tu as trouvé le nombre mystère, il est égal à %d",proposition";
+	printf ("Bravo tu as trouvé le nombre mystère, il est égal à %d\n",proposition);
+	return 0;
 }
